fb0f13fdfc81418771b831cac7b7afd05be3a6bc
Zain ul abdain <zainburfat112@gmail.com>
2023-11-29 12:32:40 +0400

    updates

--- resources/views/content/index.blade.php
+++ resources/views/content/index.blade.php
@@ -1,6 +1,64 @@
-</head>
+@extends('layouts/layoutMaster')
 
-<body>
+@section('title', 'User Management')
+
+@section('vendor-style')
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/datatables-bs5/datatables.bootstrap5.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/datatables-buttons-bs5/buttons.bootstrap5.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/select2/select2.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/animate-css/animate.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/sweetalert2/sweetalert2.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/toastr/toastr.css') }}" />
+@endsection
+
+@section('vendor-script')
+    <script src="{{ asset('assets/vendor/libs/moment/moment.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/select2/select2.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/sweetalert2/sweetalert2.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/toastr/toastr.js') }}"></script>
+@endsection
+
+@section('page-script')
+<script>
+    const getPermissions = @json(auth()->user()->roles);
+    const getDirectPermissions = @json(auth()->user()->permissions);
+    const userRole = getDirectPermissions.length ? 'super-admin' : getPermissions[0].name;
+    const userPermissions = getDirectPermissions.length ? [] : getPermissions[0].permissions;
+
+    function hasPermission(permissionName) {
+        if (userRole === 'super-admin') {
+            return true;
+        }
+
+        return userPermissions.some(permission => permission.name === permissionName);
+    }
+
+
+</script>
+    <script src="{{ asset('js/custom.js') }}"></script>
+    <script src="{{ asset('js/users.js') }}"></script>
+    <script>
+        $(function() {
+            @if (session('message'))
+                @if (session('status'))
+                    toastr.success("{{ session('message') }}");
+                @else
+                    toastr.error("{{ session('message') }}");
+                @endif
+            @endif
+
+            @if ($errors->any())
+                @foreach ($errors->all() as $error)
+                    toastr.error("{{ $error }}");
+                @endforeach
+            @endif
+        });
+    </script>
+@endsection
+
+@section('content')
     <div class="chat">
         <!-- Header -->
         <div class="top">
@@ -71,4 +129,4 @@
     });
 </script>
 
-</html>
+@endsection
